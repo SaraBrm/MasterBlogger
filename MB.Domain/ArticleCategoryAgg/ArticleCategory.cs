5b44f35974bf2a8b1b58ba6fbcfd95b88abf5681
sarabrm <sarabayrami01@gmail.com>
2023-03-06 09:25:20 -0800

    Init repository- article category management implemented

---
+++ MB.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,49 @@
+﻿using MB.Domain.ArticleCategoryAgg.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory
+    {
+        public long Id { get; private set; }
+        public string Title { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime CreationDate { get; set; }
+
+        public ArticleCategory(string title, IArticleCategoryValidatorService validatorService)
+        {
+            GuardAgainstEmptyTitle(title);
+            validatorService.CheckThatThisRecordAlreadyExists(title);
+            Title = title;
+            IsDeleted = false;
+            CreationDate = DateTime.Now;
+        }
+
+        public void GuardAgainstEmptyTitle(string title)
+        {
+            if(string.IsNullOrWhiteSpace(title))
+                throw new ArgumentNullException();
+        }
+
+        public void Rename(string title)
+        {
+            GuardAgainstEmptyTitle(title);
+            Title=title;
+        }
+
+        public void Remove()
+        {
+            IsDeleted = true;
+        }
+
+        public void Activate()
+        {
+            IsDeleted = false;
+        }
+
+    }
+}
---
+++ MB.Domain/ArticleCategoryAgg/Services/ArticleCategoryValidatorService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MB.Domain.ArticleCategoryAgg.Services
+{
+    public class ArticleCategoryValidatorService: IArticleCategoryValidatorService
+    {
+        private readonly IArticleCategoryRepository _articleCategoryRepository;
+
+        public ArticleCategoryValidatorService(IArticleCategoryRepository articleCategoryRepository)
+        {
+            _articleCategoryRepository = articleCategoryRepository;
+        }
+
+        public void CheckThatThisRecordAlreadyExists(string title)
+        {
+            if (_articleCategoryRepository.Exists(title))
+                throw new DuplicateWaitObjectException("This record already exists in database.");
+        }
+    }
+}
